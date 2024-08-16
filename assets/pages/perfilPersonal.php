1149034727767bdf95b3fed27b9356acaa564cee
Santiago Torrens <stc.enprend@hotmail.com>
2024-08-16 20:47:23 -0300

    Principalmente creacion de pagina personal del usuario, imagenes para la misma y estilos correspondientes

---
+++ assets/pages/perfilPersonal.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil Personal</title>
+    <link rel="stylesheet" href="/proyecto-sigto/assets/styles/index.css">
+    <link rel="stylesheet" href="/proyecto-sigto/assets/styles/perfilPersonal.css">
+</head>
+<body>
+<header>
+        <div id="logo-sigto">
+            <a href="/proyecto-sigto/assets/pages/index.html"><img id="logo" src="/proyecto-sigto/assets/img/logo-sigto.png" alt="logo-Sigto"></a>
+         </div>
+        <div class="menu-primero">
+            
+            <div id="menu"> 
+                <img src="/proyecto-sigto/assets/img/menu (1).png" alt="menu-burguer">
+            </div>
+            <div id="logo-sigto-dos">
+                <a href="/proyecto-sigto/assets/pages/index.html"><img id="logo" src="/proyecto-sigto/assets/img/logo-sigto.png" alt="logo-Sigto"></a>
+             </div>
+            <div>
+                <input type="text" name="buscador"> <!-- Poner img lupa dentro del input -->
+            </div>
+            <div>
+                <a href="#">Mis Compras</a>
+            </div>
+            <div>
+                <a href="/proyecto-sigto/assets/pages/cuenta.html">Mi Cuenta</a>
+            </div>
+            <div>
+                <a href="#"><img src="/proyecto-sigto/assets/img/shopping-cart (2).png" alt="carrito"></a>
+            </div>
+        </div>
+        <div class="menu-segundo">
+            <ul>
+                <li><a href="#">Todas las categorias</a></li>
+                <li><a href="/proyecto-sigto/assets/pages/ofertasSemanales.html">Ofertas de la semana</a></li>
+                <li><a href="#">Como comprar</a></li>
+                <li><a href="#">Costos y tarifas</a></li>
+                <li><a href="#">Garantia de entrega</a></li>
+                <li><a href="/proyecto-sigto/assets/pages/administrar.php">Administrar</a></li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <article class="container-info">
+            <section class="info">
+                <div class="container-contenido">
+                    <div class="img-perfil"><img src="https://img.freepik.com/foto-gratis/retro-hombre-moderno_23-2147960990.jpg?t=st=1723849586~exp=1723853186~hmac=3060b3659549a75a227d3307a72485800d3e4a5c1d1453d8f976467d97e6d2bd&w=740" alt="foto-de-perfil"></div>
+                    <div>    
+                        <h2>Santiago Torrens</h2>
+                        <p>stc@hotmail.com</p>
+                    </div>
+                </div>
+            </section>
+        </article>
+        <article class="container-info">
+            <section class="info">
+                <div class="container-contenido">
+                    <div><img src="/proyecto-sigto/assets/img/info-personal.png" alt="logo-perfil"></div>
+                    <div>    
+                        <h3>Tu informacion</h3>
+                        <p>La informacion que completas para usar tu cuenta.</p>
+                    </div>
+                    <div>
+                        <img src="/proyecto-sigto/assets/img/flecha.png" alt="flecha">
+                    </div>
+                </div>
+                <div class="container-contenido">
+                    <div><img src="/proyecto-sigto/assets/img/perfil-datos.png" alt="tarjeta-informacion"></div>
+                    <div>    
+                        <h3>Datos de cuenta</h3>
+                        <p>Datos que representan a la cuenta de Mercado Libre y Mercado Pago.</p>
+                    </div>
+                    <div>
+                        <img src="/proyecto-sigto/assets/img/flecha.png" alt="flecha">
+                    </div>
+                </div>
+                <div class="container-contenido">
+                    <div><img src="/proyecto-sigto/assets/img/seguridad.png" alt="seguridad"></div>
+                    <div>    
+                        <h3>Seguridad</h3>
+                        <p>Tienes configuraciones pendientes.</p>
+                    </div>
+                    <div>
+                        <img src="/proyecto-sigto/assets/img/flecha.png" alt="flecha">
+                    </div>
+                </div>
+                <div class="container-contenido">
+                    <div><img src="/proyecto-sigto/assets/img/tarjeta.png" alt="logo-perfil"></div>
+                    <div>    
+                        <h3>Tarjetas</h3>
+                        <p>Tarjetas guardadas en tu cuenta.</p>
+                    </div>
+                    <div>
+                        <img src="/proyecto-sigto/assets/img/flecha.png" alt="flecha">
+                    </div>
+                </div>
+                <div class="container-contenido">
+                    <div><img src="/proyecto-sigto/assets/img/ubicacion.png" alt="ubicacion"></div>
+                    <div>    
+                        <h3>Direcciones</h3>
+                        <p>Direcciones guardadas en tu cuenta.</p>
+                    </div>
+                    <div>
+                        <img src="/proyecto-sigto/assets/img/flecha.png" alt="flecha">
+                    </div>
+                </div>
+                <div class="container-contenido">
+                    <div><img src="/proyecto-sigto/assets/img/privacidad.png" alt="privacidad"></div>
+                    <div>    
+                        <h3>Privacidad</h3>
+                        <p>Preferencias y control sobre el uso de tus datos.</p>
+                    </div>
+                    <div>
+                        <img src="/proyecto-sigto/assets/img/flecha.png" alt="flecha">
+                    </div>
+                </div>
+                <div class="container-contenido">
+                    <div><img src="/proyecto-sigto/assets/img/mensaje.png" alt="mensajes"></div>
+                    <div>    
+                        <h3>Comunicaciones</h3>
+                        <p>Elige que tipo de informacion quieres recibir.</p>
+                    </div>
+                    <div>
+                        <img src="/proyecto-sigto/assets/img/flecha.png" alt="flecha">
+                    </div>
+                </div>
+            </section>
+        </article>
+    </main>
+    <script src="/proyecto-sigto/assets/js/index.js"></script>
+</body>
+</html>
