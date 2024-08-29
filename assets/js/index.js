08469f638a2a9b703e79f78ea4506c34f28d5980
Santiago Torrens <stc.enprend@hotmail.com>
2024-08-29 18:53:00 -0300

    eliminacion de cartel flotante para registro de empresas o compradores en index

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,3 @@ function desplegar(){
     let menuSegundo = document.querySelector(".menu-segundo");
     menuSegundo.style.display === "none"? menuSegundo.style.display="flex" : menuSegundo.style.display="none";
 }
-
